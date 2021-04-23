4e23501ad8ff47f014120314571c2f2d575653d5
JacksonCrawford <jacksoncrawford@protonmail.com>
2021-04-23 18:45:31 -0400

    Update paramaters, agent color (for > contrast), add density param

--- cAMP/model.py
+++ cAMP/model.py
@@ -1,5 +1,5 @@
 from mesa import Model
-from mesa.time import RandomActivation
+from mesa.time import RandomActivation, SimultaneousActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
@@ -14,21 +14,23 @@ import numpy as np
 from agents import SlimeAgent, cAMP
 
 class SlimeModel(Model):
-    def __init__(self, height, width, numAgents, kRate, dcDiffu, dhRes, dtRes, secRate, numCol):
+    def __init__(self, height, width, numAgents, gDense, kRate, dcDiffu, dhRes, dtRes, secRate):
         # number of agents per tile
-        self.n = 2
+        self.n = numAgents
+        # grid density
+        self.gD = gDense
         # rate of cAMP decay
-        self.k = 1
+        self.k = kRate
         # diffusion constant of cAMP
-        self.Dc = .001
+        self.Dc = dcDiffu
         # spatial resolution for cAMP simulation
-        self.Dh = .01
+        self.Dh = dhRes
         # time resolution for cAMP simulation
-        self.Dt = .01
+        self.Dt = dtRes
         # rate of cAMP secretion by an agent
-        self.f = 5
+        self.f = secRate
         # number of rows/columns in spatial array
-        self.w = 20
+        self.w = 50
 
         # height of grid
         self.height = 50
@@ -39,7 +41,7 @@ class SlimeModel(Model):
         self.j = 0
 
         # Create randomly ordered scheduler
-        self.schedule = RandomActivation(self)
+        self.schedule = SimultaneousActivation(self)
         # Create grid
         self.grid = MultiGrid(self.height, self.width, torus=False)
 
@@ -52,7 +54,9 @@ class SlimeModel(Model):
         # Initialize list of cells and slime agents
         self.cells = list()
         self.agents = list()
-
+    
+        r = 0
+    
         # Initial loop to create agents and fill agents list with them
         for (contents, x, y) in self.grid.coord_iter():
             # Create object of type cAMP
@@ -64,18 +68,28 @@ class SlimeModel(Model):
             # Place cAMP onto grid at coordinates x, y
             self.grid._place_agent((x, y), cell)
 
-            # Loop to create SlimeAgents            
-            for i in range(self.n):
-                # Create object of type SlimeAgent
-                ag = SlimeAgent([x, y], self, self.j)
-                # Add new SlimeAgent object to agents list
-                self.agents.append(ag)
-                # Place agent onto grid at coordinates x, y
-                self.grid.place_agent(ag, tuple([x, y]))
-                # Add agent to schedule
-                self.schedule.add(ag)
-                # Increment j (unique_id variable)
-                self.j += 1
+            # Loop to create SlimeAgents
+            if self.gD % 1 != 0:
+                r = random.random()
+                if r <= self.gD:
+                    for i in range(self.n):
+                        ag = SlimeAgent([x, y], self, self.j)
+                        self.agents.append(ag)
+                        self.grid.place_agent(ag, tuple([x, y]))
+                        self.schedule.add(ag)
+                        self.j += 1
+            else:
+                for i in range(self.n):
+                    # Create object of type SlimeAgent
+                    ag = SlimeAgent([x, y], self, self.j)
+                    # Add new SlimeAgent object to agents list
+                    self.agents.append(ag)
+                    # Place agent onto grid at coordinates x, y
+                    self.grid.place_agent(ag, tuple([x, y]))
+                    # Add agent to schedule
+                    self.schedule.add(ag)
+                    # Increment j (unique_id variable)
+                    self.j += 1
 
         # Create environment variable as array filled with zeros w x w
         self.env = np.zeros([self.w, self.w])
@@ -105,7 +119,7 @@ class SlimeModel(Model):
                 # Get all neighbors (excuding self)
                 neighbors = obj.getNeighbors()
                 # Wipe cNeighbors
-                cNeighbors = list()
+                cNeighbors.clear()
                 # Examine each neighbor
                 for neighbor in neighbors:
                     # Add cAMP neighbors to list
@@ -161,13 +175,14 @@ def cAMP_portrayal(agent):
     amt = 0
     if type(agent) is SlimeAgent:
         # Dictionary for setting portrayal settings of SlimeAgent agent
-        portrayal = {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Layer": 0}
+        portrayal = {"Shape": "circle", "w": 1, "h": 1, "Filled": "true", "Layer": 1, "r": .65}
         # Setting x coordinate of agent in portrayal dict
         portrayal["x"] = agent.getX()
         # Setting y coordinate of agent in portrayal dict
         portrayal["y"] = agent.getY()
-        # Setting agent color to blue 
-        portrayal["Color"] = "#357ecb"
+        # Setting agent color to red 
+#        portrayal["Color"] = "#357ecb"
+        portrayal["Color"] = "red"
     elif type(agent) is cAMP:
         # Dictionary for setting portrayal settings of cAMP agent
         portrayal = {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Layer": 0}
@@ -182,26 +197,25 @@ def cAMP_portrayal(agent):
         if amt == 0:
             portrayal["Color"] = "white"
         elif amt < 3:
-            portrayal["Color"] = "#d4d4d4"
+            portrayal["Color"] = "#c3c3c3"
         elif amt < 6:
-            portrayal["Color"] = "#a2a2a2"
+            portrayal["Color"] = "#b1b1b1"
         elif amt < 9:
-            portrayal["Color"] = "#6e6e6e"
+            portrayal["Color"] = "#a1a1a1"
         elif amt < 12:
-            portrayal["Color"] = "#5e5e5e"
+            portrayal["Color"] = "#929292"
         elif amt < 15:
-            portrayal["Color"] = "#4e4e4e"
+            portrayal["Color"] = "#868686"
         elif amt < 18:
-            portrayal["Color"] = "#3b3b3b"
+            portrayal["Color"] = "#7b7b7b"
         elif amt < 21:
-            portrayal["Color"] = "#2a2a2a"
+            portrayal["Color"] = "#6e6e6e"
         else:
-            portrayal["Color"] = "#828282"
+            portrayal["Color"] = "#646464"
 
     return portrayal
 
 # Telling mesa to create a grid with the agent
-canvas_element = CanvasGrid(cAMP_portrayal, 50, 50, 600, 600)
 
 #chart_element = ChartModule([{"Label": "Molds", "Color":"#43566c"}, {"Label":"cAMP's", "Color":"#acdabd"}])
 
@@ -210,13 +224,16 @@ model_params = {
         "height": 200,
         "width": 200,
         "numAgents": UserSettableParameter("slider", "Number of Agents", 1, 1, 10, 1),
-        "kRate": UserSettableParameter("slider", "Rate of cAMP decay", 0.5, .1, 10, .5),
-        "dcDiffu": UserSettableParameter("slider", "Diffusion Constant of cAMP", .001, .0001, .05, .005),
-        "dhRes": UserSettableParameter("slider", "Spatial Resolution for cAMP Simulation", .01, .01, 1, .01),
-        "dtRes": UserSettableParameter("slider", "Time Resolution for cAMP Simulation", .01, .001, 1, .01),
-        "secRate": UserSettableParameter("slider", "Rate of cAMP Secretion by an Agent", 5, 1, 15, 1),
-        "numCol": UserSettableParameter("slider", "Number of Rows/Columns in Spatial Array", 20, 2, 100, 2)
+        "gDense": UserSettableParameter("slider", "Density of Agents on Grid", .5, 0, 1, .1),
+        "kRate": UserSettableParameter("slider", "Rate of cAMP decay", 0.5, 0, 1, .1),
+        "dcDiffu": UserSettableParameter("slider", "Diffusion Constant of cAMP", .001, 0, .01, .001),
+        "dhRes": UserSettableParameter("slider", "Spatial Resolution for cAMP Simulation", .01, 0, .1, .01),
+        "dtRes": UserSettableParameter("slider", "Time Resolution for cAMP Simulation", .01, .01, .1, .01),
+        "secRate": UserSettableParameter("slider", "Rate of cAMP Secretion by an Agent", 5, 1, 15, 1)
         }
+
+canvas_element = CanvasGrid(cAMP_portrayal, 50, 50, 550, 550)
+
 
 # Creating ModularServer
 server = ModularServer(SlimeModel, [canvas_element], "Keller-Segel Slime Mold Aggregation Model", model_params)
